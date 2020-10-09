f2cc6a9c749a54ff050d104c3ffcc4f6d3f48b87
Nishant Gautam <eddienishant@gmail.com>
2020-10-09 22:25:34 +0530

    Added Basic template Structure with css and js

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const port = 8000;
+
 const app = express();
 
-// make the app listen
+app.use(express.urlencoded());
+app.use(express.static('assets')); // use the middleware access the static files 
 
+ // set our view engine
+ app.set('view engine','ejs');
+ app.set('views','./views');
+
+
+// setting our routes for the localhost:8000 path 
+app.get('/', function(req, res) {
+    return res.render('home'); // since we are rendering our home view
+})
+
+
+
+
+// make the app listen
 app.listen(port, function(err) {
     if(err) {
         console.log(`There is an error ${err}`)
